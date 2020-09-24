65aede341dee374ef4e4adc1a909fd5569d8cc3d
Fernando <fernando@kakao.com>
2020-09-25 01:22:25 +0900

    Update check_status.js

--- Sources/check_status.js
+++ Sources/check_status.js
@@ -1,12 +1,11 @@
 var exec = require("child_process").exec; 
+var env = Object.create(process.env);
 
-exec('ruby get-app-status.rb', function (err, stdout, stderr) {
+exec('ruby fetch_app_status.rb', {env : env}, function (err, stdout, stderr) {
     if (stdout) {
-        
         //console.log(stdout)
         var versions = JSON.parse(stdout);
         console.log(versions)
-        
     }
     else {
         console.log("There was a problem fetching the status of the app!");
