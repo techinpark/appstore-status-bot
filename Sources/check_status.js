4de58a408369afe2e5c95d9933136d7296f1b1bf
Fernando <fernando@kakao.com>
2020-09-25 01:44:07 +0900

    Save store.db for compare status

--- Sources/check_status.js
+++ Sources/check_status.js
@@ -1,14 +1,36 @@
 var exec = require("child_process").exec; 
+var dirty = require('dirty');
+var db = dirty('store.db');
+
 var env = Object.create(process.env);
 
 exec('ruby Sources/fetch_app_status.rb', {env : env}, function (err, stdout, stderr) {
     if (stdout) {
         //console.log(stdout)
-        var versions = JSON.parse(stdout);
-        console.log(versions)
+        var app = JSON.parse(stdout);
+        checkVersion(app);
     }
     else {
         console.log("There was a problem fetching the status of the app!");
         console.log(stderr);
     }
 });
+
+
+function checkVersion(app) {
+
+    var appInfoKey = "appInfo-" + app.appID; 
+    var submissionStartKey = "submissionStart" + app.appID; 
+
+    var lastAppInfo = db.get(appInfoKey); 
+    if(!lastAppInfo || lastAppInfo.status != app.status) {
+
+        console.log("status is different");
+
+        if(app.status == "Waiting For Review") {
+            db.set(submissionStartKey, new Date());
+        }
+    }
+
+    db.set(appInfoKey, app);
+}
