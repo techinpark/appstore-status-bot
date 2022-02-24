a853c94047e3dfec5992036c554a9b11d08c7036
DongHee Kang <kanglpmg@gmail.com>
2022-02-24 13:38:35 +0900

    get_app_state 수정 (#1)
    * fix: typo
    
    * feat: Update get_app_state logic from in_review, pending
    
    * feat: Add test code
    
    * feat: Add test code
    
    * feat: Add test code
    
    * feat: Add test code
    
    * feat: Add latest_version_info to fetch_app_status
    
    * fix: typo

--- Sources/check_status.js
+++ Sources/check_status.js
@@ -1,101 +1,102 @@
-const slack = require("./slack.js")
-const exec = require("child_process").exec; 
-const dirty = require('dirty');
+const slack = require("./slack.js");
+const exec = require("child_process").exec;
+const dirty = require("dirty");
 const { Octokit, App } = require("octokit");
-const request = require('request');
+const request = require("request");
 const { prependOnceListener } = require("process");
-const fs = require('fs');
+const fs = require("fs");
 const env = Object.create(process.env);
-const octokit = new Octokit({ auth: `token ${process.env.GH_TOKEN}`});
+const octokit = new Octokit({ auth: `token ${process.env.GH_TOKEN}` });
 
 getGist();
 
-exec('ruby Sources/fetch_app_status.rb', {env : env}, function (err, stdout, stderr) {
-
+exec(
+  "ruby Sources/fetch_app_status.rb",
+  { env: env },
+  function (err, stdout, stderr) {
     if (stdout) {
-        var apps = JSON.parse(stdout);
-        console.log(apps);        
-        for(let app of apps) {
-            checkVersion(app);
-        }
+      var apps = JSON.parse(stdout);
+      console.log(apps);
+      for (let app of apps) {
+        checkVersion(app);
+      }
+    } else {
+      console.log("There was a problem fetching the status of the app!");
+      console.log(stderr);
     }
-    else {
-        console.log("There was a problem fetching the status of the app!");
-        console.log(stderr);
-    }
-});
+  }
+);
 
 function checkVersion(app) {
+  var appInfoKey = "appInfo-" + app.appID;
+  var submissionStartKey = "submissionStart" + app.appID;
 
-    var appInfoKey = "appInfo-" + app.appID; 
-    var submissionStartKey = "submissionStart" + app.appID; 
+  const db = dirty("store.db").on("load", function () {
+    var lastAppInfo = db.get(appInfoKey);
+    if (!lastAppInfo || lastAppInfo.status != app.status) {
+      console.log("[*] status is different");
+      slack.post(app, db.get(submissionStartKey));
 
-    const db = dirty('store.db').on('load', function(){
-        var lastAppInfo = db.get(appInfoKey); 
-        if(!lastAppInfo || lastAppInfo.status != app.status) {
+      if (app.status == "Waiting For Review") {
+        db.set(submissionStartKey, new Date());
+      }
+    } else {
+      console.log("[*] status is same");
+    }
 
-            console.log("[*] status is different");
-            slack.post(app, db.get(submissionStartKey))
+    db.set(appInfoKey, app);
 
-            if(app.status == "Waiting For Review") {
-                db.set(submissionStartKey, new Date());
-            }
-        } 
-        else {
-            console.log("[*] status is same");  
-        }
+    fs.readFile("store.db", "utf-8", (error, data) => {
+      if (error) {
+        return console.log(error);
+      }
 
-        db.set(appInfoKey, app);
-
-        fs.readFile('store.db', 'utf-8', (error, data) => {
-            if(error) {
-                return console.log(error);
-            }
-            
-            updateGist(data);
-        });
+      updateGist(data);
     });
+  });
 }
 
 async function getGist() {
-    
-    const gist = await octokit.rest.gists.get({
-        gist_id: process.env.GIST_ID
-    }).catch(error => console.error(`[*] Unable to update gist\n${error}`));
-    if (!gist) return ; 
+  const gist = await octokit.rest.gists
+    .get({
+      gist_id: process.env.GIST_ID,
+    })
+    .catch((error) => console.error(`[*] Unable to update gist\n${error}`));
+  if (!gist) return;
 
-    const filename = Object.keys(gist.data.files)[0];
-    const rawdataURL = gist.data.files[filename].raw_url;
-    
-    const options = {
-        url: rawdataURL
-    };
+  const filename = Object.keys(gist.data.files)[0];
+  const rawdataURL = gist.data.files[filename].raw_url;
 
-    request.get(options, function(error, response, body){
-        console.log(body);
-        fs.writeFile('store.db', body, function(error){
-            if(error) {
-                return console.log(error);
-            }
-            console.log("[*] file saved!");
-        });
+  const options = {
+    url: rawdataURL,
+  };
+
+  request.get(options, function (error, response, body) {
+    console.log(body);
+    fs.writeFile("store.db", body, function (error) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("[*] file saved!");
     });
+  });
 }
 
 async function updateGist(content) {
-    
-    const gist = await octokit.rest.gists.get({
-        gist_id: process.env.GIST_ID
-    }).catch(error => console.error(`[*] Unable to update gist\n${error}`));
-    if (!gist) return ; 
+  const gist = await octokit.rest.gists
+    .get({
+      gist_id: process.env.GIST_ID,
+    })
+    .catch((error) => console.error(`[*] Unable to update gist\n${error}`));
+  if (!gist) return;
 
-    const filename = Object.keys(gist.data.files)[0];
-    await octokit.rest.gists.update({
-        gist_id: process.env.GIST_ID,
-        files: {
-          [filename]: {
-            content: content,
-          },
-        },
-      });
+  const filename = Object.keys(gist.data.files)[0];
+  await octokit.rest.gists.update({
+    gist_id: process.env.GIST_ID,
+    files: {
+      [filename]: {
+        content: content,
+      },
+    },
+  });
 }
--- Sources/slack.js
+++ Sources/slack.js
@@ -1,105 +1,109 @@
-const moment = require("moment")
-const path = require('path')
-const { IncomingWebhook } = require('@slack/webhook')
-const { I18n } = require('i18n')
+const moment = require("moment");
+const path = require("path");
+const { IncomingWebhook } = require("@slack/webhook");
+const { I18n } = require("i18n");
 
-const webhookURL = process.env.SLACK_WEBHOOK
-const language = process.env.LANGUAGE
-const i18n = new I18n()
+const webhookURL = process.env.SLACK_WEBHOOK;
+const language = process.env.LANGUAGE;
+const i18n = new I18n();
 
 i18n.configure({
-    locales: ['en','ko'],
-    directory: path.join(__dirname, '../locales')
-})
+  locales: ["en", "ko"],
+  directory: path.join(__dirname, "../locales"),
+});
 
-i18n.setLocale(language)
+i18n.setLocale(language);
 
 function post(appInfo, submissionStartDate) {
-    
-    const status = i18n.__(appInfo.status)
-    const message  = i18n.__("Message", {appname: appInfo.name, status: status})
-    const attachment = slackAttachment(appInfo, submissionStartDate)
+  const status = i18n.__(appInfo.status);
+  const message = i18n.__("Message", { appname: appInfo.name, status: status });
+  const attachment = slackAttachment(appInfo, submissionStartDate);
 
-    const params = {
-        "attachments" : [attachment],
-        "as_user" : "true"
-    }
-    
-    hook(message, attachment)
+  const params = {
+    attachments: [attachment],
+    as_user: "true",
+  };
+
+  hook(message, attachment);
 }
 
 async function hook(message, attachment) {
-    const webhook = new IncomingWebhook(webhookURL, {})
-    await webhook.send({
-        text: message,
-        attachments: [attachment],
-    });
+  const webhook = new IncomingWebhook(webhookURL, {});
+  await webhook.send({
+    text: message,
+    attachments: [attachment],
+  });
 }
 
 function slackAttachment(appInfo, submissionStartDate) {
-    const attachment = {
-        "fallback": `The status of your app ${appInfo.name} has been changed to ${appInfo.status}`,
-        "color": colorForStatus(appInfo.status),
-        "title": "App Store Connect",
-        "author_name": appInfo.name,
-        "author_icon": appInfo.iconURL,
-        "title_link": `https://appstoreconnect.apple.com/apps/${appInfo.appID}/appstore`,
-        "fields": [
-            {
-                "title": i18n.__("Version"),
-                "value": appInfo.version,
-                "short": true
-            },
-            {
-                "title": i18n.__("Status"),
-                "value": i18n.__(appInfo.status),
-                "short": true
-            }
-        ],
-        "footer": "appstore-status-bot",
-        "footer_icon": "https://icons-for-free.com/iconfiles/png/512/app+store+apple+apps+game+games+store+icon-1320085881005897327.png",
-        "ts": new Date().getTime() / 1000
-    }
+  const attachment = {
+    fallback: `The status of your app ${appInfo.name} has been changed to ${appInfo.status}`,
+    color: colorForStatus(appInfo.status),
+    title: "App Store Connect",
+    author_name: appInfo.name,
+    author_icon: appInfo.iconURL,
+    title_link: `https://appstoreconnect.apple.com/apps/${appInfo.appID}/appstore`,
+    fields: [
+      {
+        title: i18n.__("Version"),
+        value: appInfo.version,
+        short: true,
+      },
+      {
+        title: i18n.__("Status"),
+        value: i18n.__(appInfo.status),
+        short: true,
+      },
+    ],
+    footer: "appstore-status-bot",
+    footer_icon:
+      "https://icons-for-free.com/iconfiles/png/512/app+store+apple+apps+game+games+store+icon-1320085881005897327.png",
+    ts: new Date().getTime() / 1000,
+  };
 
-    // Set elapsed time since "Waiting For Review" start
-    if (submissionStartDate && appInfo.status != "Prepare for Submission" && appInfo.status != "Waiting For Review") {
-        const elapsedHours = moment().diff(moment(submissionStartDate), "hours")
-        attachment["fields"].push({
-            "title": "Elapsed Time",
-            "value": `${elapsedHours} hours`,
-            "short": true
-        })
-    }
-    return attachment
+  // Set elapsed time since "Waiting For Review" start
+  if (
+    submissionStartDate &&
+    appInfo.status != "Prepare for Submission" &&
+    appInfo.status != "Waiting For Review"
+  ) {
+    const elapsedHours = moment().diff(moment(submissionStartDate), "hours");
+    attachment["fields"].push({
+      title: "Elapsed Time",
+      value: `${elapsedHours} hours`,
+      short: true,
+    });
+  }
+  return attachment;
 }
 
 function colorForStatus(status) {
-    const infoColor = "#8e8e8e"
-    const warningColor = "#f4f124"
-    const successColor1 = "#1eb6fc"
-    const successColor2 = "#14ba40"
-    const failureColor = "#e0143d"
-    const colorMapping = {
-        "Prepare for Submission" : infoColor,
-        "Waiting For Review" : infoColor,
-        "In Review" : successColor1,
-        "Pending Contract" : warningColor,
-        "Waiting For Export Compliance" : warningColor,
-        "Pending Developer Release" : successColor2,
-        "Processing for App Store" : successColor2,
-        "Pending Apple Release" : successColor2,
-        "Ready for Sale" : successColor2,
-        "Rejected" : failureColor,
-        "Metadata Rejected" : failureColor,
-        "Removed From Sale" : failureColor,
-        "Developer Rejected" : failureColor,
-        "Developer Removed From Sale" : failureColor,
-        "Invalid Binary" : failureColor
-    }
+  const infoColor = "#8e8e8e";
+  const warningColor = "#f4f124";
+  const successColor1 = "#1eb6fc";
+  const successColor2 = "#14ba40";
+  const failureColor = "#e0143d";
+  const colorMapping = {
+    "Prepare for Submission": infoColor,
+    "Waiting For Review": infoColor,
+    "In Review": successColor1,
+    "Pending Contract": warningColor,
+    "Waiting For Export Compliance": warningColor,
+    "Pending Developer Release": successColor2,
+    "Processing for App Store": successColor2,
+    "Pending Apple Release": successColor2,
+    "Ready for Sale": successColor2,
+    Rejected: failureColor,
+    "Metadata Rejected": failureColor,
+    "Removed From Sale": failureColor,
+    "Developer Rejected": failureColor,
+    "Developer Removed From Sale": failureColor,
+    "Invalid Binary": failureColor,
+  };
 
-    return colorMapping[status]
+  return colorMapping[status];
 }
 
 module.exports = {
-    post: post
-}
+  post: post,
+};
